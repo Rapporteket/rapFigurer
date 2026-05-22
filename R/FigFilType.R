77224aae0d281fb5c4100d2abe53511a2889e2e6
Arnfinn Hykkerud Steindal <arnfinn.hykkerud.steindal@helse-nord.no>
2026-05-22 13:06:20 +0200

    minor

--- R/FigFilType.R
+++ R/FigFilType.R
@@ -12,7 +12,7 @@
 #' @param fargepalett Hvilken fargepalett skal brukes
 #' @return UtFigFil List med en del figurparametre
 #' @keywords rapporteket norgast
-#' @importFrom grDevices bmp jpeg cairo_pdf png rgb tiff win.metafile svg dev.off
+#' @importFrom grDevices bmp jpeg cairo_pdf png rgb tiff svg dev.off
 #' @export
 
 figtype <- function(
